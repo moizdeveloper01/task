d9866533a5cb7116f1ee3a796d7ff6be634709df
moizdeveloper01 <47815142+moizdeveloper01@users.noreply.github.com>
2020-09-02 01:35:54 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+
+  <form method="post" action="index.php" class="input_form">
+		<input type="text" name="task" class="task_input">
+		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+
+        <?php if (isset($errors)) { ?>
+	<p><?php echo $errors; ?></p>
+<?php } ?>
+	</form>
+  <?php 
+    
+	
+
+
+	$db = mysqli_connect("localhost", "root", "", "todo");
+
+	// insert a quote if submit button is clicked
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
+			$errors = "You must fill in the task";
+		}else{
+			$task = $_POST['task'];
+			$sql = "INSERT INTO tasks (task) VALUES ('$task')";
+			mysqli_query($db, $sql);
+			header('location: index.php');
+		}
+    }	
+    
+    if (isset($_GET['del_task'])) {
+        $id = $_GET['del_task'];
+    
+        mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+        header('location: index.php');
+    }
+
+
+?>
+
+<table>
+	<thead>
+		<tr>
+			<th>Id</th>
+			<th>Tasks</th>
+			
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php 
+		// select all tasks if page is visited or refreshed
+		$tasks = mysqli_query($db, "SELECT * FROM tasks");
+
+		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td class="task"> <?php echo $row['task']; ?> </td>
+				<td class="delete"> 
+					<a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+				</td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+</table>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
